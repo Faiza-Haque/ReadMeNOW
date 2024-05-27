347e1d4b34af4616c0a516be90e21849d020dda7
faizahaque90 <163949218+faizahaque90@users.noreply.github.com>
2024-05-27 11:28:50 -0700

    starter code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const thereadme = require('thereadme');
+thereadme.get('./enigma/README.md')
+
+    .then(response => {
+    console.log('question:', response.questions);
+});
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What was your motivation?",
+    "Why did you build this project?",
+    "What problem does it solve?",
+    "What did you learn?",
+    "What makes your project stand out?",
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    this.questions.pop();
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log('initialize app')
+}
+
 
 // Function call to initialize app
 init();
